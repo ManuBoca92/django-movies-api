e771dfe6bf7dd7e22e17be8969b9adf367ff5ab0
Ebonom Mfam <ebonom.mfam@snow.dog>
2020-05-08 19:04:38 +0200

    removed commented code

--- movies/apis/urls.py
+++ movies/apis/urls.py
@@ -1,15 +1,13 @@
 from django.conf.urls import url
+from rest_framework import routers
 
 from .views import CommentsViewSet, MoviesViewSet, MovieDetailsView, TopView
-from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register(r'movies', MoviesViewSet, basename='movies_view')
 router.register(r'comments', CommentsViewSet, basename='comments_view')
 
 urlpatterns = [
-    # url(r'movies', MoviesView.as_view(), name='movies_view'),
-    # url(r'comments', CommentsView.as_view(), name='comments_view'),
     url(r'movie-detail', MovieDetailsView.as_view(), name='movie_details_view'),
     url(r'top', TopView.as_view(), name='top_view')
 ]
