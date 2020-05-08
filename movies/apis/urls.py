dd90717378efb3774b43b941f23899c1812f6fa5
Ebonom Mfam <ebonom.mfam@snow.dog>
2020-05-08 21:39:30 +0200

    added app_name and removed basename

--- movies/apis/urls.py
+++ movies/apis/urls.py
@@ -3,9 +3,11 @@ from rest_framework import routers
 
 from .views import CommentsViewSet, MoviesViewSet, MovieDetailsView, TopView
 
+app_name = 'apis'
+
 router = routers.DefaultRouter()
-router.register(r'movies', MoviesViewSet, basename='movies_view')
-router.register(r'comments', CommentsViewSet, basename='comments_view')
+router.register(r'movies', MoviesViewSet)
+router.register(r'comments', CommentsViewSet)
 
 urlpatterns = [
     url(r'movie-detail', MovieDetailsView.as_view(), name='movie_details_view'),
