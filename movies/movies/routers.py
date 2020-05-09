025a930a908cfc233563be46f1e843211b2d5d77
Ebonom Mfam <ebonom.mfam@snow.dog>
2020-05-09 12:32:25 +0200

    removed redundant file

--- movies/movies/routers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import routers
-from django.conf.urls import url
-from apis import views
-
-router = routers.DefaultRouter()
-router.register(r'movies', views.MovieViewSet)
-router.register(r'comments', views.CommentViewSet)
-router.register(r'top', views.TopViewSet)
